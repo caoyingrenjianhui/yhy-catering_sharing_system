38e925725d827d66ed10fc6e5101eff2b35a0acf
caoyingrenjianhui <1304887337@qq.com>
2024-04-06 21:57:55 +0800

    111

--- src/main/java/com/example/controller/CommentController.java
+++ src/main/java/com/example/controller/CommentController.java
@@ -38,7 +38,7 @@ public class CommentController {
         return commentService.deleteById(id);
     }
 
-    @GetMapping("/selectByMerchantID/{id")
+    @GetMapping("/selectByMerchantID/{id}")
     public Result selectByMerchantID(@PathVariable Integer merchantID){
         return commentService.selectByMerchantID(merchantID);
     }
